50eaa3ec4cf1e598016187acc3b78ea7a227c974
Hossain Ahmed <hossainahmed2002@gmail.com>
2025-04-07 20:49:05 +0100

    added nav bar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,44 @@
+import { Button, Container, Flex, HStack, Text } from '@chakra-ui/react'
+import React from 'react'
+import { BsMoon, BsPlusSquare, BsSun } from 'react-icons/bs'
+import { Link } from 'react-router-dom'
+import { useColorMode } from './ui/color-mode'
+
+const Navbar = () => {
+const {colorMode, toggleColorMode} = useColorMode()
+
+  return (
+    <Container maxW={"1140px"} px={4}>
+        <Flex
+            h={16}
+            alignItems={"center"}
+            justifyContent={"space-between"}
+            flexDir={{
+                base:"column",
+                sm:"row"
+            }}
+        >
+            <Text
+                fontSize={{ base: "22", sm: "28" }}
+                fontWeight={"bold"}
+                textTransform={"uppercase"}
+                textAlign={"center"}
+            
+            ><Link to={"/"}>Barter App</Link></Text>
+            <HStack spacing={2} alignItem ={"center"}>
+                <Link to={"/create"}>
+                    <Button>
+                        <BsPlusSquare fontSize={20}/>
+                    </Button>
+                </Link>
+               <Button onClick={toggleColorMode}>
+                    {colorMode === "light" ? <BsMoon /> : <BsSun />}
+                </Button>
+            </HStack>
+        </Flex>
+        </Container>
+
+  )
+}
+
+export default Navbar
